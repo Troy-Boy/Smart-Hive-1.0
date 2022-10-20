9a7924ad18030b3265a398b7922c33b9db6e50e2
Troy-Boy <achille.lanctots@gmail.com>
2022-10-20 14:34:56 -0400

    tada

--- python/2.2.2_Thermistor.py
+++ python/2.2.2_Thermistor.py
@@ -14,10 +14,11 @@ def loop():
 		analogVal = ADC0834.getResult()
 		Vr = 5 * float(analogVal) / 255
 		Rt = 10000 * Vr / (5 - Vr)
-		temp = 1/(((math.log(Rt / 10000)) / 3950) + (1 / (273.15+25)))
-		Cel = temp - 273.15
-		Fah = Cel * 1.8 + 32
-		print ('Celsius: %.2f C  Fahrenheit: %.2f F' % (Cel, Fah))
+		print(Rt)
+		# temp = 1/(((math.log(Rt / 10000)) / 3950) + (1 / (273.15+25)))
+		# Cel = temp - 273.15
+		# Fah = Cel * 1.8 + 32
+		# print ('Celsius: %.2f C  Fahrenheit: %.2f F' % (Cel, Fah))
 		time.sleep(0.2)
 
 if __name__ == '__main__':
