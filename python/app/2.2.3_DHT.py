ef1d4f91c4a9adb0a575fe7ec596914185696f99
Troy-Boy <achille.lanctots@gmail.com>
2022-11-29 18:25:29 -0500

    added lcd display

--- python/app/2.2.3_DHT.py
+++ python/app/2.2.3_DHT.py
@@ -26,7 +26,7 @@ STATE_DATA_FIRST_PULL_DOWN = 3
 STATE_DATA_PULL_UP = 4
 STATE_DATA_PULL_DOWN = 5
 
-def readDht11():
+def read_dht11():
 	GPIO.setup(dhtPin, GPIO.OUT)
 	GPIO.output(dhtPin, GPIO.HIGH)
 	time.sleep(0.05)
@@ -37,6 +37,10 @@ def readDht11():
 	unchanged_count = 0
 	last = -1
 	data = []
+	lengths = []
+	current_length = 0
+
+	# acquire input
 	while unchanged_count < MAX_UNCHANGE_COUNT:
 		current = GPIO.input(dhtPin)
 		data.append(current)
@@ -47,43 +51,33 @@ def readDht11():
 			unchanged_count += 1
 
 	state = STATE_INIT_PULL_DOWN
-
-	lengths = []
-	current_length = 0
-
+	
+	# wtf 
 	for current in data:
 		current_length += 1
 
 		if state == STATE_INIT_PULL_DOWN:
 			if current == GPIO.LOW:
 				state = STATE_INIT_PULL_UP
-			else:
-				continue
-		if state == STATE_INIT_PULL_UP:
+		elif state == STATE_INIT_PULL_UP:
 			if current == GPIO.HIGH:
 				state = STATE_DATA_FIRST_PULL_DOWN
-			else:
-				continue
-		if state == STATE_DATA_FIRST_PULL_DOWN:
+		elif state == STATE_DATA_FIRST_PULL_DOWN:
 			if current == GPIO.LOW:
 				state = STATE_DATA_PULL_UP
-			else:
-				continue
-		if state == STATE_DATA_PULL_UP:
+		elif state == STATE_DATA_PULL_UP:
 			if current == GPIO.HIGH:
 				current_length = 0
 				state = STATE_DATA_PULL_DOWN
-			else:
-				continue
-		if state == STATE_DATA_PULL_DOWN:
+		elif state == STATE_DATA_PULL_DOWN:
 			if current == GPIO.LOW:
 				lengths.append(current_length)
 				state = STATE_DATA_PULL_UP
-			else:
-				continue
+		else: 
+			continue
+
 	if len(lengths) != 40:
-		#print ("Data not good, skip")
-		return (None, None)
+		return ()
 
 	shortest_pull_up = min(lengths)
 	longest_pull_up = max(lengths)
@@ -92,39 +86,43 @@ def readDht11():
 	the_bytes = []
 	byte = 0
 
+	# convert to byte
 	for length in lengths:
 		bit = 0
 		if length > halfway:
 			bit = 1
 		bits.append(bit)
-	#print ("bits: %s, length: %d" % (bits, len(bits)))
 	for i in range(0, len(bits)):
 		byte = byte << 1
-		if (bits[i]):
-			byte = byte | 1
-		else:
-			byte = byte | 0
+		byte = byte | 1 if bits[i] else byte | 0
 		if ((i + 1) % 8 == 0):
 			the_bytes.append(byte)
 			byte = 0
-	#print (the_bytes)
+
+	# check if transmission is ok
 	checksum = (the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3]) & 0xFF
 	if the_bytes[4] != checksum:
-		#print ("Data not good, skip")
-		return (None, None)
+		return ()
 
 	return the_bytes[0], the_bytes[2]
 
+def setup_lcd1602():
+	LCD1602.init(0x27, 1)	# init(slave address, background light)
+	LCD1602.write(0, 0, 'HelloWorld!')
+	LCD1602.write(1, 1, 'Welcom to SmartHive')
+	time.sleep(2)
 
 def main():
-	print('Hello world!')
-
+	setup_lcd1602()	
 	while True:
-		result = readDht11()
+		result = read_dht11()
 		if result:
 			humidity, temperature = result
-			print ("humidity: %s %%,  Temperature: %s C`" % (humidity, temperature))
-		time.sleep(1)
+		LCD1602.clear()
+		LCD1602.write(0, 0, f'Humidity: {humidity}%')
+		LCD1602.write(0, 1, f'Temperature: {temperature} C')
+		time.sleep(2) # takes samples each 2 sec
+		LCD1602.clear()
 
 
 def destroy():
